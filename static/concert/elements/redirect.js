c4c166c573780183eb4b46ee9eaa8c9153ff030f
kiwik8 <gouverneur.martin@hotmail.com>
2023-01-22 19:53:14 +0100

    added function in js

--- static/concert/elements/redirect.js
+++ static/concert/elements/redirect.js
@@ -1,3 +1,32 @@
+function getStyle(el, styleProp) {
+    var value, defaultView = (el.ownerDocument || document).defaultView;
+    // W3C standard way:
+    if (defaultView && defaultView.getComputedStyle) {
+      // sanitize property name to css notation
+      // (hypen separated words eg. font-Size)
+      styleProp = styleProp.replace(/([A-Z])/g, "-$1").toLowerCase();
+      return defaultView.getComputedStyle(el, null).getPropertyValue(styleProp);
+    } else if (el.currentStyle) { // IE
+      // sanitize property name to camelCase
+      styleProp = styleProp.replace(/\-(\w)/g, function(str, letter) {
+        return letter.toUpperCase();
+      });
+      value = el.currentStyle[styleProp];
+      // convert other units to pixels on IE
+      if (/^\d+(em|pt|%|ex)?$/i.test(value)) { 
+        return (function(value) {
+          var oldLeft = el.style.left, oldRsLeft = el.runtimeStyle.left;
+          el.runtimeStyle.left = el.currentStyle.left;
+          el.style.left = value || 0;
+          value = el.style.pixelLeft + "px";
+          el.style.left = oldLeft;
+          el.runtimeStyle.left = oldRsLeft;
+          return value;
+        })(value);
+      }
+      return value;
+    }
+  }
 function copyStringToClipboard (str) {
     // Create new element
     var el = document.createElement('textarea');
@@ -14,24 +43,38 @@ function copyStringToClipboard (str) {
     // Remove temporary element
     document.body.removeChild(el);
     }
-
+function getOut() {
+    window.location.href = url;
+}
 var menuButton = document.getElementById("menu-button");
 var menu = document.getElementById("menu");
 var i = false
+const sleep = ms => new Promise(r => setTimeout(r, ms));
+visibility = getStyle(menuButton, "visibility");
+console.log(visibility);
+if (visibility == "hidden") {
+    setTimeout(getOut, 2000);
+}
+else{
+    setTimeout(getOut, 20000);
+}
 
 
 menuButton.addEventListener("click", function(event) {
     if (i == false) {
-    event.preventDefault();
-    menu.style.display = "flex";
-    copyStringToClipboard(url)
-    i = true
-} else {
-    event.preventDefault();
-    menu.style.display = "none";
-    i = false
-}
+        event.preventDefault();
+        menu.style.display = "flex";
+        copyStringToClipboard(url);
+        i = true;
+    } 
+    else {
+        event.preventDefault();
+        menu.style.display = "none";
+        i = false;
+    }
 });
+
+
 
 
 
